fe7dd993cb528e93b86e72a6f92a8ec523e490ff
BismuthBetel <97366538+BismuthBetel@users.noreply.github.com>
2022-04-04 22:55:31 -0500

    added functions for creating event, auditing time and event delegation for textarea

--- Develop/assets/js/file.js
+++ Develop/assets/js/file.js
@@ -1,2 +1,40 @@
 var events = {};
 
+var eventTableEl = document.querySelector(".event-body");
+
+// create event input
+
+var createEvent = function (eventText) {
+
+
+    var eventParagraph = $("<p>").addClass("textarea").text(eventText);
+
+    //append p element to hard coded td element
+
+    $(".event-body").append(eventParagraph);
+}
+
+var auditTime = function (eventTime) {
+    // get time from span element in table element
+    var time = $(eventTime).find("span").text().trim();
+}
+//Time blocks should highlight depending on if its in the past, present, or future
+
+// when clicking on <p> element should convert to textarea
+
+$(".event-body").on("click", "p", function () {
+    var text = $(this).text().trim();
+
+    var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+//When clicking in another place besides text area, the text should be saved
+
+//When saved, the data is saved to localStorage and is persistent
+
+createEvent();
